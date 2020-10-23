ec21989549cc04c1fc4a99eabf1f348211393d0d
aleksandrchernogorov <60900369+aleksandrchernogorov@users.noreply.github.com>
2020-10-24 00:20:21 +0500

    Sync refactored

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,8 @@ import { User } from './models/User';
 
 const user = new User({ name: 'NewRecord', age: 0 });
 
-user.save();
+user.events.on('change', () => {
+  console.log('change occured')
+})
+
+user.events.trigger('change');
