df7668bc3a39899afa6e746a3b08903ccd8ca5cc
admin <admin@admins-MacBook-Air.local>
2020-10-30 22:09:33 +0500

    One more

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
-import { User } from './models/User';
-const collection = User.buildUserCollection();
+import { UserForm } from './views/UserForm';
 
-collection.on('change', () => {
-  console.log(collection);
-});
-collection.fetch();
+const userForm = new UserForm(document.getElementById('root'));
+
+userForm.render();
